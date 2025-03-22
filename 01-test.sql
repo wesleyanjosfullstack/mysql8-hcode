06da4d1e2b9bb886f707928b8178a96a9668e6bd
WESLEY ANJOS <wesleyanjosfullstack@gmail.com>
2025-03-22 17:12:28 -0300

    adicionando arquivo sql

---
+++ 01-test.sql
@@ -0,0 +1,12 @@
+show databases;
+use sakila;
+show tables;
+select * from city;
+select * from city limit 100;
+
+create database if not exists test;
+use test;
+show tables;
+use mysql;
+show tables;
+select * from user;
